f373d6692490c309221b27f1473ed93b1c0eb16c
amrithadevi <sangeethameganathan98@gmail.com>
2019-02-22 16:12:20 +0530

    Create Nfactorial.c

---
+++ Nfactorial.c
@@ -0,0 +1,10 @@
+if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;           
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
